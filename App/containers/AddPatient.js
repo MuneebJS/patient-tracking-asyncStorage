88b191b26b120e0acfdeed5089f83b59157a4e02
Meen Joy <munibkhan7737@gmail.com>
2017-08-27 22:14:39 -0700

    initial commit

---
+++ App/containers/AddPatient.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput, Button, StyleSheet, FlatList, Picker, AsyncStorage } from 'react-native';
+import styles from '../style'
+
+
+
+
+export default class AddPatient extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            nameText: '',
+            nameDesease: '',
+            gender: 'male',
+            senLevel: 'normal',
+            description: 'n/a'
+        };
+        this.watchGender = this.watchGender.bind(this);
+        this.watchGender = this.watchLevel.bind(this);
+        this.setItem = this.setItem.bind(this);
+    }
+
+    static navigationOptions = {
+        title: 'Add Patient',
+    };
+
+    addData() {
+        let date = new Date();
+        console.log(date)
+        let today = date.getDate();
+        let month = date.getMonth();
+        let year = date.getFullYear();
+        let fullDate = today + '/' + month + '/' + year;
+        // console.log(today, month, year, fullDate);
+        let patientData = {
+            patient: {
+                name: this.state.nameText,
+                date: fullDate,
+                desease: this.state.nameDesease,
+                gender: this.state.gender,
+                senLevel: this.state.senLevel,
+                description: this.state.description,
+                setData: []
+            }
+        }
+
+        this.setItem(patientData);
+
+    }
+    async componentDidMount() {
+        try {
+            const value = await AsyncStorage.getItem('Patients');
+            if (value !== null) {
+                // We have data!!
+                parsedVal = JSON.parse(value);
+                // console.log(parsedVal);
+                this.setState({
+                    setData: parsedVal
+                })
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log('error get item', error);
+        }        
+    }
+
+    async setItem(obj) {
+
+        arrayToPushedData = this.state.setData;
+        arrayToPushedData.push(obj);
+
+        try {
+            await AsyncStorage.setItem('Patients', JSON.stringify(arrayToPushedData));
+            console.log(arrayToPushedData);
+
+        }
+        catch (error) {
+            // Error saving data
+            console.log('error in setItem', error)
+        }
+    }
+
+    async consoleDate() {
+        try {
+            const value = await AsyncStorage.getItem('Patients');
+            if (value !== null) {
+                // We have data!!
+                parsedVal = JSON.parse(value);
+                console.log(parsedVal);
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log('error get item', error);
+        }
+    }
+
+    watchGender = (gender) => {
+        this.setState({ gender: gender })
+    }
+    watchLevel = (level) => {
+        this.setState({ senLevel: level })
+    }
+
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text style={styles.margin20}>Patient Name</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: '#eee', borderWidth: 0, }}
+                    onChangeText={(text) => this.setState({ nameText: text })}
+                    placeholder="Name"
+                />
+
+                <Text style={styles.margin20}>Name of Desease</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: '#eee', borderWidth: 0, }}
+                    onChangeText={(text) => this.setState({ nameDesease: text })}
+                    placeholder="Desease"
+                />
+                <Text style={styles.margin10}>Gender</Text>
+                <Picker style={{}} selectedValue={this.state.gender} onValueChange={(item) => this.watchGender(item)}>
+                    <Picker.Item label="Male" value="male" />
+                    <Picker.Item label="Female" value="female" />
+                    <Picker.Item label="Other" value="other" />
+                </Picker>
+
+                <Text style={styles.margin10}>Level of sensitivity</Text>
+                <Picker style={{}} selectedValue={this.state.senLevel} onValueChange={(item) => this.watchLevel(item)}>
+                    <Picker.Item label="Normal" value="normal" />
+                    <Picker.Item label="High" value="high" />
+                    <Picker.Item label="Low" value="low" />
+                </Picker>
+                <Text style={styles.margin20}>Description</Text>
+                <TextInput multiline={true}
+                    style={{ height: 40, borderColor: '#eee', borderWidth: 0, }}
+                    onChangeText={(text) => this.setState({ description: text })}
+                    placeholder="Desease"
+                />
+                <Button title="Add" style={{
+                    flex: 1
+                }} onPress={this.addData.bind(this)} />
+                <Button style={{
+                    flex: 1
+                }} title="Show" onPress={this.consoleDate.bind(this)} />
+            </View>
+        )
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import AddPatient from './App/containers/AddPatient.js';
+import ShowPatient from './App/containers/ShowPatient.js';
+import Home from './App/components/Home.js';
+import { TabNavigator, StackNavigator } from "react-navigation";
+
+// export default class patient_tracking_AsyncStorage extends Component {
+//   render() {
+//     return (
+      
+//     );
+//   }
+// }
+
+export default class App extends Component {
+  
+    static navigationOptions = {
+      title: 'Home',
+    };
+    // showPatNav () {
+    //   this.props.navigation.navigate('ShowPatient');
+    // }
+    render() {
+  
+      const { navigate } = this.props.navigation;
+      return (
+        
+        <View>
+             <Home addPatNav={() => navigate('AddPatient')} showPatNav={() => navigate('ShowPatient')} />
+  
+               {/* <Text>Hll</Text> */}
+        </View>
+      );
+    }
+  }
+  
+  const MainScreenNavigator = TabNavigator({
+  
+    Home: { screen: App },
+    AddPatient: { screen: AddPatient },
+    YourPatient: { screen: ShowPatient },
+  });
+  
+  const PrimaryNavigation = StackNavigator({
+    // Home: {screen: App},
+    Home: {screen: MainScreenNavigator},
+    AddPatient: {screen: AddPatient},
+    ShowPatient: {screen: ShowPatient}
+  })
+  
+
+
+AppRegistry.registerComponent('patient_tracking_AsyncStorage', () => PrimaryNavigation);
